73ccf52e412b4b7824a9799374cd25f8c6b94e20
xiukingv <105408441+xiukingv@users.noreply.github.com>
2024-12-14 13:39:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["把永远爱你写进诗的结尾", "你是我唯一想要的了解","刘诗琴阳晓金"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
